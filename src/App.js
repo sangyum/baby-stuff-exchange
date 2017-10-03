5297b9397fd07ed64d04e26ff67f7c1a53ef0c31
Charlene <cbestiva@gmail.com>
2017-10-03 10:24:29 -0700

    added react native material kit

--- src/App.js
+++ src/App.js
@@ -1,6 +1,14 @@
 import React from "react";
 import { StyleSheet, Text, View } from "react-native";
+import { MKButton } from 'react-native-material-kit'
 
+const ColoredRaisedButton = MKButton.coloredButton()
+  .withText('SIGN IN')
+  .withOnPress(() => {
+    console.log('HI')
+  })
+  .build()
+  
 export default class App extends React.Component {
   render() {
     return (
@@ -9,6 +17,7 @@ export default class App extends React.Component {
         <Text>Open up App.js to start working on your app!</Text>
         <Text>Changes you make will automatically reload.</Text>
         <Text>Shake your phone to open the developer menu.</Text>
+        <ColoredRaisedButton />
       </View>
     );
   }
